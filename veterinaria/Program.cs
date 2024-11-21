e0bea8aaac5c3c866ec6be66cf4ead3210cb9ca3
Jackemp <jackpaalomino@gmail.com>
2024-11-21 10:50:23 -0500

    MENU

--- veterinaria/Program.cs
+++ veterinaria/Program.cs
@@ -17,4 +17,36 @@ sus respectivos arreglos
 Divida entre sus integrantes, el lider consolida y los integrantes uno hace lo de servicios y el otro el de mascotas
 Debe tener una rama por integrante, el lider debe tener por lo menos 2 ramas: main y dev. debe trabajar y consolidar en dev
 */
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        while (true)
+        {
+            Console.WriteLine("1. Crear Servicio");
+            Console.WriteLine("2. Eliminar Servicio");
+            Console.WriteLine("3. Listar Servicios");
+            Console.WriteLine("4. Crear Mascota");
+            Console.WriteLine("5. Eliminar Mascota");
+            Console.WriteLine("6. Listar Mascotas");
+            Console.WriteLine("7. Crear Boleta");
+            Console.WriteLine("8. Listar Boletas");
+            Console.WriteLine("0. Salir");
+
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "0":
+                    return;
+            }
+        }
+    }
+}
